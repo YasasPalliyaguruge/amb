8bfba655425f126dbbec400b3f70512ade7d6814
YasasPalliyaguruge <developer.yasas@gmail.com>
2026-04-23 05:22:40 +0530

    Hide loading Spline backdrop plane

--- src/components/cinematic/PublicSplinePreloader.tsx
+++ src/components/cinematic/PublicSplinePreloader.tsx
@@ -1,6 +1,8 @@
 import SplineScene from './SplineScene';
+import type { Application } from '@splinetool/runtime';
 
 const PUBLIC_LOADING_SPLINE_SCENE = 'https://prod.spline.design/fi4kVao3vk5K2oVj/scene.splinecode';
+const LOADING_SPLINE_BACKDROP_ID = '6083c9f1-1e5b-4f8d-bb61-89e24484a04d';
 
 type PublicSplinePreloaderProps = {
   isExiting: boolean;
@@ -8,6 +10,17 @@ type PublicSplinePreloaderProps = {
 };
 
 export default function PublicSplinePreloader({ isExiting, onSceneReady }: PublicSplinePreloaderProps) {
+  const handleSceneReady = (app: Application) => {
+    const backdrop = app.findObjectById(LOADING_SPLINE_BACKDROP_ID) ?? app.findObjectByName('Rectangle 2');
+
+    if (backdrop) {
+      backdrop.visible = false;
+      app.requestRender();
+    }
+
+    onSceneReady?.();
+  };
+
   return (
     <div
       className={`public-spline-preloader ${isExiting ? 'public-spline-preloader--leaving' : ''}`}
@@ -18,7 +31,7 @@ export default function PublicSplinePreloader({ isExiting, onSceneReady }: Publi
         scene={PUBLIC_LOADING_SPLINE_SCENE}
         className="public-spline-preloader__scene"
         decorative
-        onLoad={onSceneReady}
+        onLoad={handleSceneReady}
       />
     </div>
   );
